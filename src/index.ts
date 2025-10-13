6cf955fec0fcb70af59a44c630e39a34352d00ba
Denis <kuzmichdenis21@gmail.com>
2025-10-13 19:04:13 +0300

    fixing vercel errors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import {setupApp} from "./setup-app";
 
-const app = express();
+export const app = express();
 setupApp(app);
 
-// const PORT = process.env.PORT || 3000;
-//
-// app.listen(PORT, () => {
-//     console.log(`Example app listening on port ${PORT}`);
-// });
+if (process.env.VERCEL !== '1') {
+    const PORT = process.env.PORT || 3000;
 
+    app.listen(PORT, () => {
+        console.log(`Example app listening on port ${PORT}`);
+    });
+}
 export default app;
