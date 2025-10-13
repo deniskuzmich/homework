5e7e062531ab2071510cce37085cef1be9651914
Denis <kuzmichdenis21@gmail.com>
2025-10-13 19:00:31 +0300

    fixing vercel errors

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const setup_app_1 = require("./setup-app");
-exports.app = (0, express_1.default)();
-(0, setup_app_1.setupApp)(exports.app);
-const PORT = process.env.PORT || 3000;
-exports.app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
-});
+const app = (0, express_1.default)();
+(0, setup_app_1.setupApp)(app);
+// const PORT = process.env.PORT || 3000;
+//
+// app.listen(PORT, () => {
+//     console.log(`Example app listening on port ${PORT}`);
+// });
+exports.default = app;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import {setupApp} from "./setup-app";
 
-export const app = express();
+const app = express();
 setupApp(app);
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+//
+// app.listen(PORT, () => {
+//     console.log(`Example app listening on port ${PORT}`);
+// });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
-});
+export default app;
