813a7fc33f287cf59fb3a141a63a626eb3af76c5
Denis <kuzmichdenis21@gmail.com>
2025-10-14 21:55:00 +0300

    fixing vercel errors

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export const app = express();
 setupApp(app);
 
 if (process.env.VERCEL !== "1") {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
   });
