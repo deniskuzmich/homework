0c4f933824be168182dbb7663983274a560ee7eb
Denis <kuzmichdenis21@gmail.com>
2025-10-29 00:25:24 +0300

    hm1-3 blogs and Posts ready

--- src/handlers/blogs-hadlers/get-blogs-list.hanlder.ts
+++ src/handlers/blogs-hadlers/get-blogs-list.hanlder.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import { blogsRepository } from "../../respositories/blogs-repository";
 import { HTTP_STATUSES } from "../../http_statuses/http_statuses";
+import {mapToBlogViewModel} from "../mappers/map-to-blog-view-model";
 
 export async function getBlogsListHandler(req: Request, res: Response) {
   try {
     const foundBlogs = await blogsRepository.findBlogs()
-    res.status(HTTP_STATUSES.OK_200).send(foundBlogs);
+    const blogsViewModel = foundBlogs.map(mapToBlogViewModel)
+    res.status(HTTP_STATUSES.OK_200).send(blogsViewModel);
   } catch (err) {
     res.sendStatus(HTTP_STATUSES.INTERNAL_SERVER_ERROR_500)
   }
