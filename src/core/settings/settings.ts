4840bb194a43e715b35c78070534ba4fd48bd15c
Denis <kuzmichdenis21@gmail.com>
2025-10-28 23:49:39 +0300

    hm1-3 blogs and Posts ready

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -1,6 +1,6 @@
 export const SETTINGS = {
   PORT: process.env.PORT || 5000,
   MONGO_URL:
-    process.env.MONGO_URL || 'mongodb://localhost:27017/',
+    process.env.MONGO_URL || 'mongodb+srv://root:root@cluster0.rfbjknn.mongodb.net/?appName=Cluster0',
   DB_NAME: process.env.DB_NAME || 'Blogs-and-Posts',
 };
