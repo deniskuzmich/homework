8c472aeab324612267a748465395ed158c19d762
Denis <kuzmichdenis21@gmail.com>
2025-11-07 22:41:25 +0300

    hm-1-4 - ready

--- dist/core/middlewares/validation/posts.validation-middleware/posts.input.validation-middleware.js
+++ dist/core/middlewares/validation/posts.validation-middleware/posts.input.validation-middleware.js
@@ -1,34 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.postInputValidation = void 0;
+exports.postInputValidation = exports.blogIdValidation = exports.contentValidation = exports.shortDescriptionValidation = exports.titleValidation = void 0;
 const express_validator_1 = require("express-validator");
-const titleValidation = (0, express_validator_1.body)("title")
+exports.titleValidation = (0, express_validator_1.body)("title")
     .isString()
     .withMessage("title is not correct")
     .trim()
     .isLength({ min: 1, max: 30 })
     .withMessage("title length is not correct");
-const shortDescriptionValidation = (0, express_validator_1.body)("shortDescription")
+exports.shortDescriptionValidation = (0, express_validator_1.body)("shortDescription")
     .isString()
     .withMessage("shortDescription is not correct")
     .trim()
     .isLength({ min: 1, max: 100 })
     .withMessage("shortDescription length is not correct");
-const contentValidation = (0, express_validator_1.body)("content")
+exports.contentValidation = (0, express_validator_1.body)("content")
     .isString()
     .withMessage("content is not correct")
     .trim()
     .isLength({ min: 1, max: 1000 })
     .withMessage("content length is not correct");
-const blogIdValidation = (0, express_validator_1.body)("blogId")
+exports.blogIdValidation = (0, express_validator_1.body)("blogId")
     .exists()
     .withMessage("blogId is required")
     .trim()
     .isString()
     .withMessage("blogId must be a string");
 exports.postInputValidation = [
-    titleValidation,
-    shortDescriptionValidation,
-    contentValidation,
-    blogIdValidation,
+    exports.titleValidation,
+    exports.shortDescriptionValidation,
+    exports.contentValidation,
+    exports.blogIdValidation,
 ];
--- dist/routers/post.router.js
+++ dist/routers/post.router.js
@@ -11,9 +11,10 @@ const id_validation_middleware_1 = require("../core/middlewares/validation/id.va
 const input_validation_result_middleware_1 = require("../core/middlewares/validation/input.validation-result.middleware");
 const posts_input_validation_middleware_1 = require("../core/middlewares/validation/posts.validation-middleware/posts.input.validation-middleware");
 const super_admin_guard_middleware_1 = require("../auth/super-admin.guard.middleware");
+const pagination_validation_1 = require("../common/validation/pagination-validation");
 exports.postRouter = (0, express_1.Router)();
 exports.postRouter
-    .get("", get_posts_list_hanlder_1.getPostsListHanlder)
+    .get("", pagination_validation_1.paginationValidation, get_posts_list_hanlder_1.getPostsListHanlder)
     .get("/:id", id_validation_middleware_1.idValidation, input_validation_result_middleware_1.inputValidationResultMiddleware, get_post_hanlder_1.getPostHandler)
     .put("/:id", super_admin_guard_middleware_1.superAdminGuardMiddleware, id_validation_middleware_1.idValidation, posts_input_validation_middleware_1.postInputValidation, input_validation_result_middleware_1.inputValidationResultMiddleware, update_post_hanlder_1.updatePostHanlder)
     .post("", super_admin_guard_middleware_1.superAdminGuardMiddleware, posts_input_validation_middleware_1.postInputValidation, input_validation_result_middleware_1.inputValidationResultMiddleware, post_posts_hanlder_1.postPostsHandler)
