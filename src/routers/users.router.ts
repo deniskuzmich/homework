6c402e3efe285ca17fed280af89cca90236abf04
Denis <kuzmichdenis21@gmail.com>
2025-11-14 22:43:57 +0300

    hm-2-1 - ready

--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -7,12 +7,13 @@ import {idValidation} from "../core/middleware-validation/id.validation-middlewa
 import {deleteUserHandler} from "../users/handlers/delete-user";
 import {paginationValidation} from "../common/validation/pagination-validation";
 import {userInputValidation} from "../users/middleware-validation/users-input.validation";
+import {inputValidationResultMiddleware} from "../core/middleware-validation/input.validation-result.middleware";
 
 export const usersRouter = Router();
 
 usersRouter
-  .get("", superAdminGuardMiddleware, userInputDtoValidation,paginationValidation, getAllUsers)
+  .get("", superAdminGuardMiddleware, userInputDtoValidation,paginationValidation, inputValidationResultMiddleware,getAllUsers)
 
-  .post("", superAdminGuardMiddleware,userInputValidation, createUserHandler)
+  .post("", superAdminGuardMiddleware,userInputValidation, inputValidationResultMiddleware,createUserHandler)
 
-  .delete('/:id', superAdminGuardMiddleware,  idValidation, deleteUserHandler )
+  .delete('/:id', superAdminGuardMiddleware,  idValidation, inputValidationResultMiddleware,deleteUserHandler )
