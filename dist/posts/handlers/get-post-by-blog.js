366551d5a946e9f8a84419eaaf1675c5de515f16
Denis <kuzmichdenis21@gmail.com>
2025-12-04 18:50:00 +0300

    HM 2-2 queryRepo

--- dist/posts/handlers/get-post-by-blog.js
+++ dist/posts/handlers/get-post-by-blog.js
@@ -16,6 +16,8 @@ const post_for_blog_mapper_1 = require("../../blogs/mapper/post-for-blog-mapper"
 function getPostByBlogIdHanlder(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         const query = (0, post_for_blog_mapper_1.valuesPaginationMaper)(req.query);
+        if (!req.params.id)
+            return null;
         const post = yield posts_query_repository_1.postsQueryRepository.getPostByBlogId(req.params.id, query);
         if (!post) {
             return res.sendStatus(http_statuses_1.HttpStatuses.NotFound);
--- src/posts/handlers/get-post-by-blog.ts
+++ src/posts/handlers/get-post-by-blog.ts
@@ -4,8 +4,8 @@ import {postsQueryRepository} from "../repository/posts-query-repository";
 import {valuesPaginationMaper} from "../../blogs/mapper/post-for-blog-mapper";
 
 export async function getPostByBlogIdHanlder(req: Request, res:Response) {
-
   const query = valuesPaginationMaper(req.query);
+  if(!req.params.id) return null;
 
   const post = await postsQueryRepository.getPostByBlogId(req.params.id, query)
   if(!post) {
