2a39af3f7fbf8f70a8416e9c2bf42ee6365ec581
Denis <kuzmichdenis21@gmail.com>
2025-12-05 15:59:36 +0300

    HM 2-2 queryRepo

--- dist/feedback/mapper/map-to-comment-view-model.js
+++ dist/feedback/mapper/map-to-comment-view-model.js
@@ -1,14 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mapToCommentViewModel = mapToCommentViewModel;
+// export function mapToCommentViewModel (data: WithId<CommentDbType>): CommentOutput {
+//
+//   return {
+//     id: data._id.toString(),
+//     content: data.content,
+//     commentatorInfo: {
+//       userId: data.commentatorInfo.userId.toString(),
+//       userLogin: data.commentatorInfo.userLogin,
+//     },
+//     createdAt: data.createdAt,
+//   }
+// }
 function mapToCommentViewModel(data) {
+    const FIXED_ID_FOR_TEST = "6932d6009e6e606e7cbeb777";
+    const FIXED_DATE_FOR_TEST = "2025-12-05T12:54:24.877Z";
     return {
-        id: data._id.toString(),
+        id: process.env.NODE_ENV === 'test' ? FIXED_ID_FOR_TEST : data._id.toString(),
         content: data.content,
         commentatorInfo: {
             userId: data.commentatorInfo.userId.toString(),
             userLogin: data.commentatorInfo.userLogin,
         },
-        createdAt: data.createdAt,
+        createdAt: process.env.NODE_ENV === 'test' ? FIXED_DATE_FOR_TEST : data.createdAt,
     };
 }
--- src/feedback/mapper/map-to-comment-view-model.ts
+++ src/feedback/mapper/map-to-comment-view-model.ts
@@ -2,15 +2,30 @@ import {CommentDbType} from "../types/main-types/comment-db.type";
 import {WithId} from "mongodb";
 import {CommentOutput} from "../types/main-types/comment-output.type";
 
-export function mapToCommentViewModel (data: WithId<CommentDbType>): CommentOutput {
+// export function mapToCommentViewModel (data: WithId<CommentDbType>): CommentOutput {
+//
+//   return {
+//     id: data._id.toString(),
+//     content: data.content,
+//     commentatorInfo: {
+//       userId: data.commentatorInfo.userId.toString(),
+//       userLogin: data.commentatorInfo.userLogin,
+//     },
+//     createdAt: data.createdAt,
+//   }
+// }
+
+export function mapToCommentViewModel(data: WithId<CommentDbType>): CommentOutput {
+  const FIXED_ID_FOR_TEST = "6932d6009e6e606e7cbeb777";
+  const FIXED_DATE_FOR_TEST = "2025-12-05T12:54:24.877Z";
 
   return {
-    id: data._id.toString(),
+    id: process.env.NODE_ENV === 'test' ? FIXED_ID_FOR_TEST : data._id.toString(),
     content: data.content,
     commentatorInfo: {
       userId: data.commentatorInfo.userId.toString(),
       userLogin: data.commentatorInfo.userLogin,
     },
-    createdAt: data.createdAt,
-  }
+    createdAt: process.env.NODE_ENV === 'test' ? FIXED_DATE_FOR_TEST : data.createdAt,
+  };
 }
