54e7f59bf611e210ec6fbca7a978d431ff7d2eea
Denis <kuzmichdenis21@gmail.com>
2025-12-29 12:24:50 +0300

    HM 3-1

--- dist/devices/mapper/map-session-to-view-model.js
+++ dist/devices/mapper/map-session-to-view-model.js
@@ -5,7 +5,7 @@ function mapSessionToViewModel(session) {
     return {
         ip: session.ip,
         title: session.deviceName,
-        lastActiveDate: new Date(session.iat * 1000).toDateString(),
+        lastActiveDate: new Date(session.iat * 1000).toISOString(),
         deviceId: session.deviceId,
     };
 }
--- src/devices/mapper/map-session-to-view-model.ts
+++ src/devices/mapper/map-session-to-view-model.ts
@@ -4,7 +4,7 @@ export function mapSessionToViewModel(session: SessionType) {
   return {
       ip: session.ip,
       title: session.deviceName,
-      lastActiveDate: new Date(session.iat * 1000).toDateString(),
+      lastActiveDate: new Date(session.iat * 1000).toISOString(),
       deviceId: session.deviceId,
   }
 }
