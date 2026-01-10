e6d72a5bae4b839d86fb3e0adcc7fe3137a25862
Denis <kuzmichdenis21@gmail.com>
2026-01-10 12:53:35 +0300

    HM 3-2

--- dist/adapters/email-examples.js
+++ dist/adapters/email-examples.js
@@ -9,9 +9,18 @@ exports.emailExamples = {
  </p>`;
     },
     passwordRecovery(code) {
-        return `<h1>Password recovery</h1>
-    <p>To finish password recovery please follow the link below:
-      <a href='https://somesite.com/password-recovery?recoveryCode=${code}'>recovery password</a>
-    </p>`;
+        return `
+    <h1>Password recovery</h1>
+    <p>To finish password recovery please follow the link below:</p>
+    <a href="https://some-front.com/new-password?recoveryCode=${code}">
+      recovery password
+    </a>
+  `;
     }
+    // passwordRecovery(code: string) {
+    //   return `<h1>Password recovery</h1>
+    //   <p>To finish password recovery please follow the link below:
+    //     <a href='https://somesite.com/password-recovery?recoveryCode=${code}'>recovery password</a>
+    //   </p>`
+    // }
 };
