3a9ab5b89b20eb87465530f8cf96fdabdedacf9f
Denis <kuzmichdenis21@gmail.com>
2026-01-18 23:55:49 +0300

    HM 3-3 mongoose complited

--- src/comments/handler/like.handler.ts
+++ src/comments/handler/like.handler.ts
@@ -3,7 +3,7 @@ import {CommentsService} from "../service/comments.service";
 import {inject, injectable} from "inversify";
 import {ResultStatus} from "../../common/types/result.status";
 import {mapResultCodeToHttpExtension} from "../../common/mapper/mapResultCodeToHttpExtention";
-import {HttpStatuses} from "../../common/types/http-statuses";
+
 
 
 @injectable()
@@ -16,16 +16,15 @@ export class LikeHandler {
 
   async updateLikeStatus(req: Request, res: Response) {
     const commentId = req.params.commentId;
-    if (!commentId) {
-      return res.sendStatus(HttpStatuses.NotFound)
-    }
-    // const userId = req.user!.userId;
 
+    // const userId = req.user!.userId;
 
     const result = await this.commentsService.updateLikeForComment(commentId)
 
     if(result.status === ResultStatus.Success)  {
       return res.status(mapResultCodeToHttpExtension(result.status)).send(result.extensions)
     }
+
+    return res.sendStatus(mapResultCodeToHttpExtension(result.status))
   }
 }
